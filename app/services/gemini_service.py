a4969f79cf365a759e2ea16fd2ac9929b3381c80
ahmedcmpunkx@gmail.com <ametwalli@acctsonair.com>
2024-11-28 04:12:10 +0200

    readme

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -8,5 +8,8 @@ genai.configure(api_key=settings.GEMINI_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-pro') # Selected a free model
 
 def gemini_response(text):
+    #TODO: Eliminate the markdowns from the text
+    #TODO: Make sure to have a limit for the token (input)
+    #TODO: Make sure to have a pagination for the output
     response = model.generate_content(text)
     return response.text
